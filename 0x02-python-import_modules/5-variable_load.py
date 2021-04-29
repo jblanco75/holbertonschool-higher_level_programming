591add330a95b64d001c018717c594a41f772fcd
jblanco75 <jblanco75@gmail.com>
2021-04-29 00:08:41 +0000

    import modules

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-import sys
 from variable_load_5 import a
 
 if __name__ == '__main__':
-    if (len(sys.argv) - 1) == 0:
-        print(a)
-    else:
-        print(sys.argv)
+    print(a)
