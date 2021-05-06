eb178d75a7a816e322325332462cd1292441a30e
jblanco75 <jblanco75@gmail.com>
2021-05-06 04:09:24 +0000

    DS: lists, tuples

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,8 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return None
-    if idx > len(my_list):
+    if idx < len(my_list):
+        my_list[idx] = element
+        return my_list
+    else:
         return None
-    my_list[idx] = element
-    return my_list
