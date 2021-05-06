e31373019593e70fda4944358ebbbcfc7c8f475b
jblanco75 <jblanco75@gmail.com>
2021-05-06 23:13:24 +0000

    DS sets,dict

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
-        new_list = my_list[:]
-        for i in my_list:
-            new_list[search] = replace
-        return new_list
+    return list(map(lambda i: i if i != search else replace, my_list))
