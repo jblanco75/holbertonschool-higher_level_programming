eb0d7ba1daad8c03962ff16ba9d41df54e861004
jblanco75 <jblanco75@gmail.com>
2021-05-06 04:43:17 +0000

    DS: lists, tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,4 +4,6 @@ def multiple_returns(sentence):
     if sentence:
         length = len(sentence)
         first = sentence[0]
-    return length, first
+        return length, first
+    else:
+        return length, None
