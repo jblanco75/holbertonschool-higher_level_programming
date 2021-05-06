d88a5230c99299cf17806f4e261df5babe3144e4
jblanco75 <jblanco75@gmail.com>
2021-05-06 04:25:05 +0000

    DS: lists, tuples

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,8 +2,9 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     if idx < 0:
-        return my_list[:]
-    if idx > len(my_list):
-        return my_list[:]
-    new_list[idx] = element
-    return new_list
+        return new_list
+    if idx < len(my_list):
+        new_list[idx] = element
+        return new_list
+    else:
+        return new_list
