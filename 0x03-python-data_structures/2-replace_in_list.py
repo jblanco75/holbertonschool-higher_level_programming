1bce5ea7a664333e1318ba91989ee6013b7073b8
jblanco75 <jblanco75@gmail.com>
2021-05-06 04:13:19 +0000

    DS: lists, tuples

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return None
-    if idx < len(my_list):
-        my_list[idx] = element
-        return my_list
-    else:
-        return my_list
+    if my_list:
+        if idx < 0:
+            return None
+        if idx < len(my_list):
+            my_list[idx] = element
+            return my_list
+        else:
+            return my_list
