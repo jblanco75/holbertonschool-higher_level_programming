bbdf3107e495a23edcbddf5a26c421fca17dfd6c
jblanco75 <jblanco75@gmail.com>
2021-05-18 21:37:28 +0000

    Classes-objects

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Create private instance __size"""
     def __init__(self, size=0):
-        if size != type(int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
