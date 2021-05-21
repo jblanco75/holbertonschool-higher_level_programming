b2f5527e41132efe104ab33aaa47e0112ba7fe7e
jblanco75 <jblanco75@gmail.com>
2021-05-21 02:50:09 +0000

    More classes

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -85,4 +85,4 @@ class Rectangle():
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() < rect_2.area():
             return rect_2
-        return rect_1A
+        return rect_1
