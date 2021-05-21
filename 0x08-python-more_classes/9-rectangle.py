5e021b32a984b8909f0a28c189d165c95acebc5e
jblanco75 <jblanco75@gmail.com>
2021-05-21 03:32:14 +0000

    More classes

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -11,9 +11,9 @@ class Rectangle():
 
     def __init__(self, width=0, height=0):
         """width of Rectangle"""
-        self.__width = width
+        self.width = width
         """height of Rectangle"""
-        self.__height = height
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
