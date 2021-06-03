f962640c892cd77760141dcaa6d848df9522a70d
jblanco75 <jblanco75@gmail.com>
2021-06-03 02:52:49 +0000

    Inheritance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Square class that inherits from
+   Rectangle (9-rectangle.py)"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Inherits from class Rectangle"""
+    def __init__(self, size):
+        """Constructor of Instantiation"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Returns area of the square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return Square Description"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
