efe31ae22b9d91772d46196bd3b10c4be6923ce5
jblanco75 <jblanco75@gmail.com>
2021-06-08 21:31:52 +0000

    Almost a circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,3 +26,35 @@ class Square(Rectangle):
         message = "[Square] ({:d}) {:d}/{:d} - {:d}".format(
             self.id, self.x, self.y, self.size)
         return message
+
+    def update(self, *args, **kwargs):
+        """Updates the class assigning a key/value
+        argument to attributes"""
+        elem = 0
+        if args is not None and len(args) != 0:
+            for arg in args:
+                if elem == 0:
+                    self.id = arg
+                elif elem == 1:
+                    self.size = arg
+                elif elem == 2:
+                    self.x = arg
+                elif elem == 3:
+                    self.y = arg
+                elem += 1
+
+        if kwargs is not None and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        """Returns dictionary representation of the Square"""
+        return ({"id": self.id, "size": self.width,
+                 "x": self.x, "y": self.y})
