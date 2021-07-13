b7b4767deb772a23544c8b873124887bb9579a07
jblanco75 <jblanco75@gmail.com>
2021-07-13 21:07:33 +0000

    show grants

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- lists all privileges of the MySQL users on your server
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES; 
+SHOW GRANTS FOR 'user_0d_2'@'localhost'; 
