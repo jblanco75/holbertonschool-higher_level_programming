0e044ed45c773364111b2ea6786bab0e11c86e3e
jblanco75 <jblanco75@gmail.com>
2021-07-14 03:21:29 +0000

    count by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,4 @@ SELECT tv_genres.name AS genre, count(tv_show_genres.genre_id) AS number_of_show
 FROM tv_genres INNER JOIN tv_show_genres 
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
-ORDER BY number_of_shows;
+ORDER BY number_of_shows DESC;
