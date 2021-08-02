87a92a418c9737d9eca9a12125ab5bd7f718b7af
jblanco75 <jblanco75@gmail.com>
2021-08-02 23:50:42 +0000

    loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,8 +2,6 @@
 
 const multi = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-let text = '';
 for (let i = 0; i < multi.length; i++) {
-  text += multi[i];
   console.log(multi[i]);
 }
