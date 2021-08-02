79487b31e89147c09a064e1e1dd7e4ed25ae1a49
jblanco75 <jblanco75@gmail.com>
2021-08-02 21:48:35 +0000

    args and argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (process.argv.length === 2) {
-  console.log('No Arguments');
+  console.log('No Argument');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
