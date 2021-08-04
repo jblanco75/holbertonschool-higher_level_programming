470c85867e8707740b0e3ee51d472154e3c18a7a
jblanco75 <jblanco75@gmail.com>
2021-08-04 02:16:25 +0000

    advanced

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
 const list = require('./100-data');
-console.log(list);
-console.log(list.map((index, value) => index * value));
+const list1 = require('./100-data').list;
+const list2 = list1.map((index, value) => index * value);
+console.log(list1);
+console.log(list2);
