68900a2e8c4086a565337398e0f64dd99aa59c16
jblanco75 <jblanco75@gmail.com>
2021-08-19 22:36:10 +0000

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Find a peak"""
+
+
+def find_peak(list_of_integers):
+    """Func to find a peak in a list"""
+    if not list_of_integers:
+        return None
+    else:
+        return sorted(list_of_integers)[-1]
