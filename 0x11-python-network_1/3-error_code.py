26454e6ba4a8c9081e3ef3548fc4c1c251c6b30a
jblanco75 <jblanco75@gmail.com>
2021-08-20 23:17:05 +0000

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and
+ displays the body of the response (decoded in utf-8)"""
+
+
+from sys import argv
+import urllib.request
+import urllib.error
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            print(response.read().decode("utf-8"))
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
