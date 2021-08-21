5a6c947f7263ec57c2af3062ed3c357c05684ea4
jblanco75 <jblanco75@gmail.com>
2021-08-21 00:48:56 +0000

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python script that takes in a letter and sends a POST request
+ to URL with the letter as a parameter"""
+
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    q = sys.argv[1] if len(argv) > 1 else ""
+    try:
+        r = requests.post('http://0.0.0.0:5000/search_user',
+                          data={'q': q}).json()
+        if 'id' in r and 'name' in r:
+            print("[{}] {}".format(r['id'], r['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
