5cfc003e56d0832be40c2cc70d258793df08d634
jblanco75 <jblanco75@gmail.com>
2021-09-15 02:20:26 +0000

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,10 +10,10 @@ request.get(url, function (err, response, body) {
   if (err) {
     console.log(err);
   }
-  const data = JSON.parse(body);
-  for (let i = 0; data.results[i] !== undefined; i++) {
-    if (data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-      count++;
+  const movie = JSON.parse(body);
+  for (let i = 0; movie.results[i] !== undefined; i++) {
+    if (movie.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+      count += 1;
     }
   }
   console.log(count);
